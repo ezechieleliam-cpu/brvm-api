11cbf32f55e893c965abb4713c1e3f12ec625992
ezechieleliam-cpu <ezechieleliam@gmail.com>
2025-09-17 12:37:35 +0100

    Update server.py

--- server.py
+++ server.py
@@ -26,6 +26,7 @@ def home():
 
 
 @app.route("/market/stocks")
+def market_stocks():
 print(f"📥 Requête reçue à /market/stocks à {datetime.now().isoformat()}")
     return jsonify(get_brvm_stocks())
 

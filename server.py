3596ede139542245c167147f0bbeedfaed1a4078
Ezechiel Eliam <ezechieleliam@gmail.com>
2025-09-17 10:02:46 +0100

    Enable CORS for frontend access

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 from fetch_brvm_data import get_brvm_data_with_ssl
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/api/brvm")
 def brvm():

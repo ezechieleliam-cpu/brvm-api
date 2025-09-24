f1c9c4b9e6d8eefe1e9e5dfd0219924c32be5385
ezechieleliam-cpu <ezechieleliam@gmail.com>
2025-09-24 21:34:24 +0100

    Update server.py

--- server.py
+++ server.py
@@ -89,5 +89,6 @@ def favicon():
 
 # 🚀 Lancement du serveur
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # ✅ Compatible Render
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+

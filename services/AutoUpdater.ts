6aaff6923a39e3d6e90cf14b174b0ac92eec668f
Ezechiel Eliam <ezechieleliam@gmail.com>
2025-10-05 19:18:04 +0100

    Initial BRVM API

--- services/AutoUpdater.ts
+++ services/AutoUpdater.ts
@@ -2,6 +2,14 @@ import { isMarketOpen } from './MarketScheduler';
 import { scrapeBRVM, scrapeRichBourse } from './BRVMScraper';
 import { fetchNews } from './NewsAggregator';
 import { cache } from '../cache';
+import mongoose from 'mongoose';
+import { StockModel } from './models/Stock';
+
+mongoose.connect(process.env.MONGO_URI!);
+
+for (const stock of brvmData) {
+  await StockModel.create({ ...stock, timestamp: new Date() });
+}
 
 export async function autoUpdate() {
   if (!isMarketOpen()) {
